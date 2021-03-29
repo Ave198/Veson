61f927827c55ef43da6f5ef8c4981bfa3637a97b
Ave198 <81591025+Ave198@users.noreply.github.com>
2021-03-30 00:42:22 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,48 +1,10 @@
-/*
- *   Copyright (c) 2020 routerabfrage
- *   All rights reserved.
- *   https://github.com/routerabfrage/License
- */
-// jshint esversion: 8
-const Discord = require("discord.js");
-const client = new Discord.Client();
+index.js
+const Discord - require("discord.js");
+const Client - new Discord.Client;
+Client.on("ready" , (console.log ("bot opérationnel")) => {});
 
-const fs = require("fs");
 
-const Enmap = require("enmap");
 
-const config = require("./config.json");
-client.config = config;
 
-client.commands = new Enmap();
-
-console.log("------------------------------------------------");
-
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error;
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    const evt = require(`./events/${file}`);
-    let evtName = file.split(".")[0];
-    console.log(`Loaded event '${evtName}'`);
-    client.on(evtName, evt.bind(null, client));
-  });
-  console.log("------------------------------------------------");
-});
-
-fs.readdir("./commands/", async (err, files) => {
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let cmdName = file.split(".")[0];
-    console.log(`Loaded Command '${cmdName}'`);
-    client.commands.set(cmdName, props);
-  });
-  console.log("------------------------------------------------");
-});
-
-process.on("unhandledRejection", (error) => {
-  console.error("Unhandled promise rejection:", error);
-});
-
-client.login(config.token || process.env.TOKEN);
+Client.login ("ODI2MTU2Mzc1ODk2NzUyMTI4.YGIYFQ.ESxFbfanT8Yum3TRj2Th2KScGQE
+");
