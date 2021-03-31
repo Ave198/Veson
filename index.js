a2b9b6d3674fba5e4b77497eb547ae9ce1babebc
Ave198 <81591025+Ave198@users.noreply.github.com>
2021-03-31 14:36:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js')
-const bot = new Discord.Client()
+const veson = new Discord.Client()
 
-bot.on('ready', () => {
+veson.on('ready', () => {
     console.log('Connecté!')})
-bot.on('message', message =>{
+veson.on('message', message =>{
   if(message.content.startsWith('ping)){
   message.channel.send('pong!')
      };
      })
           Client.login(process.env.BOT_TOKEN)
 
-    bot.login(process.env.BOT_TOKEN)
+    veson.login(process.env.BOT_TOKEN)
